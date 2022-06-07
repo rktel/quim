4a8ae834316909a42d20da736af9deb7b25e11dc
rktel <rakinakuy@hotmail.com>
2022-06-07 13:58:11 -0500

    Names_TEST

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+const Names = new Mongo.Collection('test');
 
 Meteor.startup(() => {
   // code to run on server at startup
   console.log("METEOR MONGO_URL: " + process.env.MONGO_URL);
+  console.log("NAMES COLLECTION: " + Names);
 });
