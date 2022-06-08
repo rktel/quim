0af17f08db240fad2d1b161aa8fde8440e5efa81
rktel <rakinakuy@hotmail.com>
2022-06-08 18:40:51 -0500

    devices.insert'

--- imports/api/methods/index.js
+++ imports/api/methods/index.js
@@ -1,7 +1,14 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+
+const Devices = new Mongo.Collection('devices');
 
 Meteor.methods({
     'demo'(){
       console.log("--DEMO--");
+    },'devices.insert'(device){
+        console.log("--devices.insert--");
+        console.log(device);
+        Devices.insert(device);
     }
 });
