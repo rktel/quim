c2986c18485cc1fa9aa10733462a36825f1a9d55
rktel <rakinakuy@hotmail.com>
2022-06-08 17:44:43 -0500

    demo3

---
+++ imports/api/methods/index.js
@@ -0,0 +1,7 @@
+import { Mongo } from 'meteor/mongo';
+
+Meteor.methods({
+    'demo'(){
+      console.log("DEMO");
+    }
+});
--- server/main.js
+++ server/main.js
@@ -1,16 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import '../imports/api/methods/index.js';
 
-const Names = new Mongo.Collection('names');
+//const Names = new Mongo.Collection('names');
 
 Meteor.startup(() => {
   // code to run on server at startup
-  console.log("METEOR MONGO_URL: " + process.env.MONGO_URL); 
-  console.log("NAMES COLLECTION: " + Names);
+ // console.log("METEOR MONGO_URL: " + process.env.MONGO_URL); 
+ // console.log("NAMES COLLECTION: " + Names);
 });
 
-Meteor.methods({
-  'demo'(){
-    console.log("DEMO");
-  }
-});
