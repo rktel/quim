b5f28cabece8266a948bd59f8d5ba4549dee9f4a
rktel <rakinakuy@hotmail.com>
2022-07-14 16:46:50 -0500

    dkit 5

--- imports/api/methods/index.js
+++ imports/api/methods/index.js
@@ -1,14 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-const Devices = new Mongo.Collection('devices');
+const Reports = new Mongo.Collection('reports');
 
 Meteor.methods({
-    'demo'(){
-      console.log("--DEMO--");
-    },'devices.insert'(device){
-        console.log("--devices.insert--");
-        console.log(device);
-        Devices.insert(device);
+  'reports.insert'(report_array) {
+    for (const key in report_array) {
+      const repo = Reports.insert(report_array[key]);
+      console.log('repor:',repo);
     }
+  }
 });
