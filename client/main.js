647de58c1c5d8cf7d2ecbf18cca25046bedaba14
rktel <rakinakuy@hotmail.com>
2022-08-02 16:16:10 -0500

    mdb5

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import App from '../imports/ui/App.svelte';
 
+import '../imports/api/mdb.min.js';
 
 Meteor.startup(() => {
   new App({
--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,19 +1,49 @@
 <script>
-	import { Reports } from '../db/Reports.js';
-	import { Meteor } from 'meteor/meteor';
-  const handler = Meteor.subscribe('reports');
+  import { Reports } from "../db/Reports.js";
+  import { Meteor } from "meteor/meteor";
+  const handler = Meteor.subscribe("reports");
   let reports = [];
-  $m:{
+  $m: {
     reports = Reports.find({}).fetch();
   }
 </script>
+
 <div>
-  {#each reports as repo (repo._id)}
-    <div>
-      <h1>{repo.imei}</h1>
-      <p>{repo.dateAndTime}</p>
-      <p>{repo.latitude}</p>
-      <p>{repo.longitude}</p>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <form class="d-flex input-group w-auto">
+        <input
+          type="search"
+          class="form-control form-control-sm rounded"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="search-addon"
+        />
+        <span class="input-group-text border-0" id="search-addon">
+          <i class="fas fa-search" />
+        </span>
+      </form>
     </div>
-  {/each}
+  </nav>
+  <table class="table table-sm table-bordered">
+    <thead>
+      <tr>
+        <th scope="col">FechaHora</th>
+        <th scope="col">latitud</th>
+        <th scope="col">Longitud</th>
+        <th scope="col">Kilometaje</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each reports as repo (repo._id)}
+      <tr>
+        <th scope="row">{repo.dateAndTime}</th>
+        <td>{repo.latitude}</td>
+        <td>{repo.longitude}</td>
+        <td>{repo.odometer}</td>
+      </tr>
+      {/each}
+    </tbody>
+  </table>
+
 </div>
