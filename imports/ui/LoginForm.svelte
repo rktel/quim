5e33948753f4fba8f088d476122a630434cfb462
rktel <rakinakuy@hotmail.com>
2022-08-03 14:48:16 -0500

    logout 2

--- imports/ui/LoginForm.svelte
+++ imports/ui/LoginForm.svelte
@@ -9,38 +9,31 @@
     }
 </script>
 
-<section class="vh-100">
-    <div class="container py-5 h-100">
-      <div class="row d-flex align-items-center justify-content-center h-100">
-        <div class="col-md-8 col-lg-7 col-xl-6">
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-            class="img-fluid" alt="Phone image">
-        </div>
-        <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-          <form on:submit|preventDefault={handleSubmit}>
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-              <input type="text" id="form1Example13" class="form-control form-control-lg" 
-              required
-              bind:value={username}/>
-              <label class="form-label" for="form1Example13">Username</label>
-            </div>
-  
-            <!-- Password input -->
-            <div class="form-outline mb-4">
-              <input type="password" id="form1Example23" class="form-control form-control-lg"
-              required
-              bind:value={password}/>
-              <label class="form-label" for="form1Example23">Password</label>
-            </div>
+<form class="login-form" on:submit|preventDefault={handleSubmit}>
+    <div>
+        <label htmlFor="username">Username</label>
 
-  
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
-
-  
-          </form>
-        </div>
-      </div>
+        <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                required
+                bind:value={username}
+        />
     </div>
-  </section>
+
+    <div>
+        <label htmlFor="password">Password</label>
+
+        <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                bind:value={password}
+        />
+    </div>
+    <div>
+        <button type="submit">Log In</button>
+    </div>
+</form>
