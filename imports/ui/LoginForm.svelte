10b89fe0864ae20676d95b51ff24b30dfaffb2f0
rktel <rakinakuy@hotmail.com>
2022-08-03 15:00:57 -0500

    accounts 4

--- imports/ui/LoginForm.svelte
+++ imports/ui/LoginForm.svelte
@@ -10,14 +10,14 @@
 </script>
 <form on:submit|preventDefault={handleSubmit}>
     <!-- Email input -->
-    <div class="form-outline mb-4">
-        <label class="form-label" htmlFor="username">Username</label>
+    <div >
+        <label  htmlFor="username">Username</label>
       <input type="text" name="username" class="form-control" bind:value={username}/>
     </div>
   
     <!-- Password input -->
-    <div class="form-outline mb-4">
-        <label class="form-label" htmlFor="password">Password</label>
+    <div >
+        <label htmlFor="password">Password</label>
       <input type="password" name="password" class="form-control" bind:value={password}/>
     </div>
   
