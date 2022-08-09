d468370427ff946ad353ce26fedbdb34865adf45
rktel <rakinakuy@hotmail.com>
2022-08-08 22:09:49 -0500

    m

--- imports/api/index.js
+++ imports/api/index.js
@@ -15,7 +15,7 @@ Meteor.methods({
   },'reports.getToday'(deviceIMEI){
     let today = new Date();
     today = today.toISOString().slice(0,10) + 'T00:00:00.000Z';
-    const reports = Reports.find({imei: Number(deviceIMEI), dateAndTime: { $gte: new Date(today) }}, { sort: { dateAndTime: -1 } }).fetch();
+    const reports = Reports.find({imei: Number(deviceIMEI), dateAndTime: { $gte: new Date(today) }}).fetch();
     return reports;
   }
 });
